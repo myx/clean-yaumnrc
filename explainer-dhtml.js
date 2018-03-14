a385b682feece60caed14a7f5c913b71b831265f
Alexander Kharichev <myx@myx.co.nz>
2018-03-15 01:14:06 +0300

    * close button text

--- explainer-dhtml.js
+++ explainer-dhtml.js
@@ -1,5 +1,6 @@
 function makeExplainer(div, parser){
     const btnClose = document.createElement("button");
+    btn.innerHTML = "CLOSE";
     btnClose.onclick(div.parentElement.removeNode(div));
     div.appendChild(btnClose);
 }
