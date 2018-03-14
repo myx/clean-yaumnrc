84e9602e79d2e82c0a379040d87ed0a040b1e71f
Alexander Kharichev <myx@myx.co.nz>
2018-03-15 01:51:59 +0300

    ! fixes

--- explainer-dhtml.js
+++ explainer-dhtml.js
@@ -1,10 +1,18 @@
 function makeExplainer(div, parser){
-    const btnClose = document.createElement("button");
-    btnClose.innerHTML = "CLOSE";
-    btnClose.onclick(div.parentElement.removeNode(div));
-    div.appendChild(btnClose);
+    const btn = document.createElement("button");
+    btn.innerHTML = "CLOSE";
+    btn.onclick = function(){
+        div.parentElement.removeNode(div);
+    };
+    div.appendChild(btn);
 }
 
 module.exports = {
+    "createPlane" : function(config){
+        const div = document.createElement("div");
+        div.cssText = "position:absolute;left:0;right:0;width:100%;height:100%;margin:0;padding:0;background-color:#fff;color:#000";
+        this.makeExplainer(div, config);
+        document.appendChild(div);
+    },
     "makeExplainer" : makeExplainer
 };
