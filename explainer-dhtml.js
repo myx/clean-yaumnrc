a9d1e9394fe12e654870a17fcd73b8212266945f
Alexander Kharichev <myx@myx.co.nz>
2018-03-15 01:29:51 +0300

    ! btn

--- explainer-dhtml.js
+++ explainer-dhtml.js
@@ -1,6 +1,6 @@
 function makeExplainer(div, parser){
     const btnClose = document.createElement("button");
-    btn.innerHTML = "CLOSE";
+    btnClose.innerHTML = "CLOSE";
     btnClose.onclick(div.parentElement.removeNode(div));
     div.appendChild(btnClose);
 }
