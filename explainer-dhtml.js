e1ab9883029942b687dc0dc3af830dcb83320350
Alexander Kharichev <myx@myx.co.nz>
2018-03-15 01:57:20 +0300

    ! CLOSE

--- explainer-dhtml.js
+++ explainer-dhtml.js
@@ -2,7 +2,7 @@ function makeExplainer(div, parser){
     const btn = document.createElement("button");
     btn.innerHTML = "CLOSE";
     btn.onclick = function(){
-        div.parentElement.removeNode(div);
+        div.remove();
     };
     div.appendChild(btn);
 }
@@ -10,7 +10,7 @@ function makeExplainer(div, parser){
 module.exports = {
     "createPlane" : function(config){
         const div = document.createElement("div");
-        div.cssText = "position:absolute;left:0;right:0;width:100%;height:100%;margin:0;padding:0;background-color:#fff;color:#000";
+        div.className = "explainer";
         this.makeExplainer(div, config);
         document.body.appendChild(div);
     },
