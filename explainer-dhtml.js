a95d5c1430ce095dd8491aa131c255fa5d473fd3
vlapan <vlapan@vlapan.com>
2020-02-13 09:20:12 +0200

    Update explainer-dhtml.js

--- explainer-dhtml.js
+++ explainer-dhtml.js
@@ -1,4 +1,4 @@
-function makeExplainer(div, config, closeFn){
+function makeExplainer(div, config, closeFn) {
     const output = document.createElement("div");
     const header = document.createElement("div");
 
@@ -8,11 +8,11 @@ function makeExplainer(div, config, closeFn){
     div.appendChild(output);
     div.appendChild(header);
 
-    function btn(name, action){
+    function btn(name, action) {
         const btn = document.createElement("button");
         btn.style = "height:2em;line-height:1em;padding:0.3em;overflow:hodden";
         btn.innerHTML = name;
-        btn.onclick = function(){
+        btn.onclick = function () {
             output.innerHTML = '';
             output.scrollTop = 0;
             output.scrollLeft = 0;
@@ -21,117 +21,173 @@ function makeExplainer(div, config, closeFn){
         header.appendChild(btn);
     }
 
-    btn("CLOSE", function(output){
+    btn("CLOSE", function (output) {
         "function" === typeof closeFn && closeFn();
         div.remove();
     });
 
-    function format(output, value){
-        if(!value){
+    function format(output, value) {
+        if (!value) {
             output.innerHTML = value;
             return;
         }
-        unrollList: if(value.list){
-            for(let i of value.list){
-                if(i === value){
+        unrollList: if (value.list) {
+            for (let i of value.list) {
+                if (i === value) {
                     break unrollList;
                 }
                 const div = document.createElement('div');
                 format(div, i);
-                if(div.innerHTML){
+                if (div.innerHTML) {
                     output.appendChild(div);
                 }
             }
             return;
         }
-        if(value.AbstractAddress){
+        if (value.AbstractAddress) {
             output.innerHTML = value.networkCidr.replace(';', '<br/>');
             return;
         }
         output.innerHTML = value;
     }
 
-    function table(output, layout){
+    function table(output, layout) {
         const table = document.createElement("table");
         table.style = "margin:2em 0;";
         table.border = "1px";
         table.cellPadding = "3";
         table.cellPadding = "2";
-        const columns = [];
-        {
+        table.columns = [];
+
+        table.filtersChanged = function () {
+            table.removeContent();
+            table.fillContent();
+        };
+
+        table.removeContent = function () {
+            for (const item of this.querySelectorAll('.content')) {
+                item.parentNode.removeChild(item);
+            }
+        };
+
+        table.onContentClick = function () {
+            const index = Array.prototype.indexOf.call(this.parentNode.children, this);
+            const input = table.querySelector('.filters th:nth-of-type(' + (index + 1) + ') input');
+            if (input.value === this.textContent) {
+                input.value = '';
+            } else {
+                input.value = this.textContent;
+            }
+            table.filtersChanged();
+        };
+
+        table.fillContent = function () {
+            const filterValues = {};
+            for (const item of table.querySelectorAll('input')) {
+                filterValues[item.columnId] = item.value;
+            }
+            let index = 0;
+            for (const row of layout.rows) {
+                const tr = document.createElement("tr");
+                tr.classList.add('content');
+                {
+                    const td = document.createElement("td");
+                    td.innerHTML = ++index;
+                    tr.appendChild(td);
+                }
+                let skip = false;
+                for (const column of this.columns) {
+                    const td = document.createElement("td");
+                    if (filterValues[column] && !~row[column].indexOf(filterValues[column])) {
+                        skip = true;
+                        break;
+                    }
+                    td.addEventListener('click', table.onContentClick);
+                    format(td, row[column]);
+                    tr.appendChild(td);
+                }
+                if (skip === false) {
+                    table.appendChild(tr);
+                }
+            }
+        };
+
+        table.fillHeaders = function () {
             const tr = document.createElement("tr");
+            const trFilters = document.createElement("tr");
+            trFilters.classList.add('filters');
             {
                 const th = document.createElement("th");
                 th.innerHTML = "#";
                 tr.appendChild(th);
+                const thFilter = document.createElement('th');
+                trFilters.appendChild(thFilter);
             }
-            for(let column of layout.columns){
+            for (const column of layout.columns) {
                 const th = document.createElement("th");
                 th.innerHTML = column.titleShort || column.title || column.name || column.id;
                 tr.appendChild(th);
-                columns.push(column.id || column.name);
+                const columnId = column.id || column.name;
+                this.columns.push(columnId);
+                const thFilter = document.createElement('th');
+                const inputFilter = document.createElement('input');
+                inputFilter.columnId = columnId;
+                inputFilter.addEventListener('input', function () {
+                    table.filtersChanged();
+                });
+                thFilter.appendChild(inputFilter);
+                trFilters.appendChild(thFilter);
             }
             table.appendChild(tr);
-        }
-        var index = 0;
-        for(let row of layout.rows){
-            const tr = document.createElement("tr");
-            {
-                const td = document.createElement("td");
-                td.innerHTML = ++index;
-                tr.appendChild(td);
-            }
-            for(let column of columns){
-                const td = document.createElement("td");
-                format(td, row[column]);
-                tr.appendChild(td);
-            }
-            table.appendChild(tr);
-        }
+            table.appendChild(trFilters);
+        };
+
+        table.fillHeaders();
+        table.fillContent();
         output.appendChild(table);
     }
 
-    btn("SSH", function(output){
+    btn("SSH", function (output) {
         table(output, config.makeSshAccessTable());
     });
 
-    btn("CONTACTS", function(output){
+    btn("CONTACTS", function (output) {
         table(output, config.makeContactsTable());
     });
 
-    btn("LOCATIONS", function(output){
+    btn("LOCATIONS", function (output) {
         table(output, config.makeLocationsTable());
     });
 
-    btn("DNS-WAN", function(output){
+    btn("DNS-WAN", function (output) {
         table(output, config.makeDnsTable(null));
     });
 
-    btn("DNS-ALL", function(output){
+    btn("DNS-ALL", function (output) {
         table(output, config.makeDnsTable());
     });
 
-    btn("MON-WAN", function(output){
+    btn("MON-WAN", function (output) {
         table(output, config.makeMonitoringTable(null));
     });
 
-    btn("MON-ALL", function(output){
+    btn("MON-ALL", function (output) {
         table(output, config.makeMonitoringTable());
     });
 
-    btn("FWD", function(output){
+    btn("FWD", function (output) {
         table(output, config.makePortForwardTable());
     });
 
 }
 
 module.exports = {
-    "createPlane" : function(config, closeFn){
+    "createPlane": function (config, closeFn) {
         const div = document.createElement("div");
         div.className = "explainer";
         div.style = "position:absolute;left:0;top:0;width:100%;height:100%;background-color:#eef;color:#000;overflow:auto";
         this.makeExplainer(div, config, closeFn);
         document.body.appendChild(div);
     },
-    "makeExplainer" : makeExplainer
+    "makeExplainer": makeExplainer
 };
